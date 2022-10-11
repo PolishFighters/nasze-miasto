7fd2c04d0dae16c40218b445a7d59b2e996d9c50
Gameefan <46563496+Gameefan@users.noreply.github.com>
2022-10-11 13:35:55 +0200

    initial boilerplate

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
+const port = (process.env.PORT || 3000);
+const fileUpload = require("express-fileupload");
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+
+ejs.fileLoader = (path) => {
+    return fs.readFileSync(path, { encoding: "utf-8" });
+};
+
+app.set({
+    "Content-Type": "text/html",
+});
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+app.set("views", "web");
+
+app.get("/", (req, res) => {
+    res.render("pages/index");
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
