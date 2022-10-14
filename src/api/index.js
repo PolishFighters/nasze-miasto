21187ff7f98699f85357d5e2d925f627b5b7835e
PolishFighter <jakubkrolak@protonmail.com>
2022-10-14 20:56:05 +0200

    -> Add singup option -> Rename card_error.ejs -> post_error.ejs -> Rename card_added.js -> post_added.js -> Fixed 'id' in db src/api/create_post.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 router.post("/login", require("./login"));
 router.get("/logout", require("./logout"));
 router.post("/create_post", require("./create_post"));
+router.post("/singup", require("./singup"));
+
 
 module.exports = router;
