e51910580e6c7b15e6ca1b0bb622eb539f3ebad5
PolishFighter <jakubkrolak@protonmail.com>
2022-10-16 00:42:13 +0200

    Add to api `unlike_post.js`

--- src/api/index.js
+++ src/api/index.js
@@ -5,5 +5,6 @@ router.get("/logout", require("./logout"));
 router.post("/create_post", require("./create_post"));
 router.post("/signup", require("./signup"));
 router.get("/like_post", require("./like_post"));
+router.get("/unlike_post", require("./unlike_post"));
 
 module.exports = router;
