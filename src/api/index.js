2298dfb1f7d1fa47ef5116b50490fef9142390b5
PolishFighter <jakubkrolak@protonmail.com>
2022-10-16 00:09:30 +0200

    Add to api `like_post.js`

--- src/api/index.js
+++ src/api/index.js
@@ -4,5 +4,6 @@ router.post("/login", require("./login"));
 router.get("/logout", require("./logout"));
 router.post("/create_post", require("./create_post"));
 router.post("/signup", require("./signup"));
+router.get("/like_post", require("./like_post"));
 
 module.exports = router;
