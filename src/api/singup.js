5f70efa59aff24d92354574d963f79f4b5b05552
Gameefan <46563496+Gameefan@users.noreply.github.com>
2022-10-15 17:31:06 +0200

    Fix the code & formatting in `signup.js`

--- src/api/singup.js
+++ src/api/singup.js
@@ -13,38 +13,36 @@ module.exports = (req, res) => {
 		res.redirect("/?msg=already_logged_in");
 		return;
 	}
-    const 
-    {
-        first_name,
-        last_name,
-        email,
-        password,
-        confirm_password,
-    } = req.body;
+	
+	const {
+		first_name,
+		last_name,
+		email,
+		password,
+		confirm_password,
+	} = req.body;
 
-    if(password != confirm_password)
-    {
-        res.redirect("/?msg=passwords_do_not_match");
-		return; 
-    }
+	if (password != confirm_password) {
+		res.redirect("/?msg=passwords_do_not_match");
+		return;
+	}
 
 	const user = db.db.users.find(v => v.email == email);
-    if (user != undefined) {
+	if (user != undefined) {
 		res.redirect("/?msg=address_is_not_available");
 		return;
 	}
 
-    db.db.users.push({
-        id: 0,
-        firstname: first_name,
-        lastname: last_name,
-        email: email,
-        password: hash(password),
-        liked: []
-    });
+	db.db.users.push({
+		id: 0,
+		firstname: first_name,
+		lastname: last_name,
+		email: email,
+		password: hash(password),
+		liked: []
+	});
 
-    db.save();
-    db.load();
-    
+	db.save();
+
 	res.redirect("/login");
 };
