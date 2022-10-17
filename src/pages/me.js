6e84aac475b5df50e7f28375a5eaba4a02586884
Gameefan <46563496+Gameefan@users.noreply.github.com>
2022-10-17 21:42:58 +0200

    Display the logged in user's posts on the `/me` endpoint

--- src/pages/me.js
+++ src/pages/me.js
@@ -1,8 +1,11 @@
+const db = require("../db");
 const sessions = require("../sessions");
 module.exports = (req, res) => {
-	if(!sessions.logged_in(req)){
+	if (!sessions.logged_in(req)) {
 		res.redirect("/login");
 		return;
 	}
-	res.render("pages/me");
+	const user = sessions.user_from_session(req.cookies.session);
+	const posts = db.db.posts.filter(v => v.author == user.id).sort((a, b) => a.created_at.getMilliseconds() - b.created_at.getMilliseconds());
+	res.render("pages/me", { posts: posts });
 };
