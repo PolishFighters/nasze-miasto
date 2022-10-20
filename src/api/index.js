eb354ba514aaadb8d0db960141f300b668199a54
Gameefan <46563496+Gameefan@users.noreply.github.com>
2022-10-20 17:20:17 +0200

    Add the ability for admins to delete posts
    Co-authored-by: Jakub Królak <PolishFighter@users.noreply.github.com>

--- src/api/index.js
+++ src/api/index.js
@@ -8,5 +8,6 @@ router.get("/like_post", require("./like_post"));
 router.get("/unlike_post", require("./unlike_post"));
 router.post("/add_comment", require("./add_comment"));
 router.post("/change_password", require("./change_password"));
+router.get("/delete_post", require("./delete_post"));
 
 module.exports = router;
