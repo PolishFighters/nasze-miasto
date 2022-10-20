94bfc0017b54b17935bf7250a4795c21f01cfebf
Gameefan <46563496+Gameefan@users.noreply.github.com>
2022-10-20 17:24:45 +0200

    Add the ability for admins to add cities
    Co-authored-by: Jakub Królak <PolishFighter@users.noreply.github.com>

--- src/api/index.js
+++ src/api/index.js
@@ -8,6 +8,7 @@ router.get("/like_post", require("./like_post"));
 router.get("/unlike_post", require("./unlike_post"));
 router.post("/add_comment", require("./add_comment"));
 router.post("/change_password", require("./change_password"));
+router.post("/add_city", require("./add_city"));
 router.get("/delete_post", require("./delete_post"));
 
 module.exports = router;
