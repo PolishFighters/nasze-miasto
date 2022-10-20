9dbd1ba831ce98ca4e41afd6945f1028736a0e30
Gameefan <46563496+Gameefan@users.noreply.github.com>
2022-10-20 17:53:24 +0200

    Allow admins to delete comments
    Co-authored-by: Jakub Królak <PolishFighter@users.noreply.github.com>

--- src/api/index.js
+++ src/api/index.js
@@ -10,5 +10,6 @@ router.post("/add_comment", require("./add_comment"));
 router.post("/change_password", require("./change_password"));
 router.post("/add_city", require("./add_city"));
 router.get("/delete_post", require("./delete_post"));
+router.get("/delete_comment", require("./delete_comment"));
 
 module.exports = router;
